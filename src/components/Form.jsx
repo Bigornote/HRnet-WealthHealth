f180729c6a6b684ca016e7cdfdaa3d69c72a7a0f
Antoine Buard <a.buard@laposte.net>
2023-04-01 20:12:12 +0200

    Correction du store avec les ; qui posait problème

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,6 +1,9 @@
 import React from "react";
+import { useDispatch } from "react-redux";
 
 const Form = () => {
+  const dispatch = useDispatch();
+
   return <div>Form</div>;
 };
 
--- src/feature/employee.slice.jsx
+++ src/feature/employee.slice.jsx
@@ -1,24 +1,24 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  employees : []
-}
+  employees: [],
+};
 
 const employeeSlice = createSlice({
-  name: "employee";
+  name: "employee",
   initialState,
   reducers: {
-    createEmployee:(state, { payload }) => {
+    createEmployee: (state, { payload }) => {
       // state = l'état actuel de notre slice
       // action = l'élément qui va être dispatcher
       // Payload = la valeur envoyé par l'action pour mettre à jour la propriété employees
       state = {
-        ...state, 
-        employees: payload;
-      }
-    }
-  }
-})
+        ...state,
+        employees: payload,
+      };
+    },
+  },
+});
 
 export const { createEmployee } = employeeSlice.actions;
 export default employeeSlice.reducer;
